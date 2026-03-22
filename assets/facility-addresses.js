701291c9054f5589f873850db613bab344584243
Pete Baston <pete@cliency.co.uk>
2026-03-22 19:11:15 +0000

    DEMO double blind pick

--- assets/facility-addresses.js
+++ assets/facility-addresses.js
@@ -281,5 +281,186 @@ window.facilityAddresses = {
     city: "Attica",
     state: "NY",
     zip: "14011"
+  },
+  // California CDCR Facilities
+  "Avenal State Prison": {
+    address: "1 Kings Way",
+    city: "Avenal",
+    state: "CA",
+    zip: "93204"
+  },
+  "California Correctional Center": {
+    address: "711-045 Center Road",
+    city: "Susanville",
+    state: "CA",
+    zip: "96127"
+  },
+  "California Correctional Institution": {
+    address: "24900 Highway 202",
+    city: "Tehachapi",
+    state: "CA",
+    zip: "93561"
+  },
+  "California Health Care Facility": {
+    address: "7707 South Soquel Drive",
+    city: "Stockton",
+    state: "CA",
+    zip: "95215"
+  },
+  "California Institution for Men": {
+    address: "14901 South Central Avenue",
+    city: "Chino",
+    state: "CA",
+    zip: "91710"
+  },
+  "California Institution for Women": {
+    address: "16756 Chino-Corona Road",
+    city: "Corona",
+    state: "CA",
+    zip: "92880"
+  },
+  "California Medical Facility": {
+    address: "1600 California Drive",
+    city: "Vacaville",
+    state: "CA",
+    zip: "95696"
+  },
+  "California Men's Colony": {
+    address: "Highway 1",
+    city: "San Luis Obispo",
+    state: "CA",
+    zip: "93409"
+  },
+  "California State Prison, Corcoran": {
+    address: "4001 King Avenue",
+    city: "Corcoran",
+    state: "CA",
+    zip: "93212"
+  },
+  "California State Prison, Los Angeles County": {
+    address: "44750 60th Street West",
+    city: "Lancaster",
+    state: "CA",
+    zip: "93536"
+  },
+  "California State Prison, Sacramento": {
+    address: "100 Prison Road",
+    city: "Represa",
+    state: "CA",
+    zip: "95671"
+  },
+  "Centinela State Prison": {
+    address: "2302 Brown Road",
+    city: "Imperial",
+    state: "CA",
+    zip: "92251"
+  },
+  "Central California Women's Facility": {
+    address: "23370 Road 22",
+    city: "Chowchilla",
+    state: "CA",
+    zip: "93610"
+  },
+  "Chuckawalla Valley State Prison": {
+    address: "19025 Wiley's Well Road",
+    city: "Blythe",
+    state: "CA",
+    zip: "92225"
+  },
+  "Corcoran State Prison": {
+    address: "4001 King Avenue",
+    city: "Corcoran",
+    state: "CA",
+    zip: "93212"
+  },
+  "Folsom State Prison": {
+    address: "300 Prison Road",
+    city: "Represa",
+    state: "CA",
+    zip: "95671"
+  },
+  "High Desert State Prison": {
+    address: "475-750 Rice Canyon Road",
+    city: "Susanville",
+    state: "CA",
+    zip: "96127"
+  },
+  "Ironwood State Prison": {
+    address: "19005 Wiley's Well Road",
+    city: "Blythe",
+    state: "CA",
+    zip: "92225"
+  },
+  "Kern Valley State Prison": {
+    address: "3000 West Cecil Avenue",
+    city: "Delano",
+    state: "CA",
+    zip: "93215"
+  },
+  "Mule Creek State Prison": {
+    address: "4001 Highway 104",
+    city: "Ione",
+    state: "CA",
+    zip: "95640"
+  },
+  "North Kern State Prison": {
+    address: "2737 West Cecil Avenue",
+    city: "Delano",
+    state: "CA",
+    zip: "93215"
+  },
+  "Pelican Bay State Prison": {
+    address: "5905 Lake Earl Drive",
+    city: "Crescent City",
+    state: "CA",
+    zip: "95531"
+  },
+  "Pleasant Valley State Prison": {
+    address: "24863 West Jayne Avenue",
+    city: "Coalinga",
+    state: "CA",
+    zip: "93210"
+  },
+  "Richard J. Donovan Correctional Facility": {
+    address: "480 Alta Road",
+    city: "San Diego",
+    state: "CA",
+    zip: "92179"
+  },
+  "Salinas Valley State Prison": {
+    address: "31625 Highway 101",
+    city: "Soledad",
+    state: "CA",
+    zip: "93960"
+  },
+  "San Quentin Rehabilitation Center": {
+    address: "San Quentin Point",
+    city: "San Quentin",
+    state: "CA",
+    zip: "94964"
+  },
+  "Sierra Conservation Center": {
+    address: "5100 O'Byrnes Ferry Road",
+    city: "Jamestown",
+    state: "CA",
+    zip: "95327"
+  },
+  "Substance Abuse Treatment Facility": {
+    address: "900 Quebec Avenue",
+    city: "Corcoran",
+    state: "CA",
+    zip: "93212"
+  },
+  "Valley State Prison": {
+    address: "21633 Avenue 24",
+    city: "Chowchilla",
+    state: "CA",
+    zip: "93610"
+  },
+  "Wasco State Prison": {
+    address: "701 Scofield Avenue",
+    city: "Wasco",
+    state: "CA",
+    zip: "93280"
   }
 };
